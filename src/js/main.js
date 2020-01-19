a200db031430b5cb141884908816a9c931064081
Kamil Baczynski <kamil-baczynski1@wp.pl>
2020-01-19 20:32:47 +0100

    Feat: Crossing out and uncrossing out list positions

--- src/js/main.js
+++ src/js/main.js
@@ -2,21 +2,31 @@
 
 // service worker registration - remove if you're not going to use it
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker.register("serviceworker.js").then(
+      function(registration) {
+        // Registration was successful
+        console.log(
+          "ServiceWorker registration successful with scope: ",
+          registration.scope
+        );
+      },
+      function(err) {
+        // registration failed :(
+        console.log("ServiceWorker registration failed: ", err);
+      }
+    );
   });
 }
 
 // place your code below
 
+//Crossing and uncrossing list positions
+const toBuyList = document.querySelector('.to-buy__list--js');
 
-console.log(`Hello world!`);
-
-
+toBuyList.addEventListener("click", e => {
+  if (e.target.className.includes('list-position__name')) {
+    e.target.classList.toggle('line-through');
+  }
+});
