5905b185fe119739cd8bbe0db4a2728787f465ac
Kamil Baczynski <kamil-baczynski1@wp.pl>
2020-01-26 18:22:06 +0100

    Feat: Local Storage memory

--- src/js/main.js
+++ src/js/main.js
@@ -22,10 +22,96 @@ if ("serviceWorker" in navigator) {
 
 // place your code below
 
-
-// List manipulations
 const toBuyList = document.querySelector('.to-buy__list--js');
 
+let key = 'shopping list';
+let shoppingList = [];
+
+// Checking is there any key in local storage
+if (localStorage.getItem(key)) {
+  
+  let local = localStorage.getItem(key);
+  shoppingList = JSON.parse(local);
+
+} else {
+  
+  let local = JSON.stringify(shoppingList);
+  localStorage.setItem(key, local);
+
+}
+
+handleList ();
+
+
+// Adding element to array
+function addArrayElement (input) {
+  shoppingList.push(input)
+
+  let local = JSON.stringify(shoppingList);
+  localStorage.setItem(key, local);
+
+  handleList();
+}
+
+
+// Removing element from array
+function removeArrayElement (element) {
+  shoppingList.splice(shoppingList.indexOf(element), 1);
+
+  let local = JSON.stringify(shoppingList);
+  localStorage.setItem(key, local);
+
+  handleList();
+}
+
+// Removing all elements from array
+function removeAllArrayElements() {
+  shoppingList = [];
+
+  let local = JSON.stringify(shoppingList);
+  localStorage.setItem(key, local);
+
+  handleList();
+}
+
+
+// Creating list elements on page from local storage data
+function handleList () {
+
+  toBuyList.innerHTML = '';
+
+  shoppingList.forEach( element => {
+
+    toBuyList.innerHTML += `
+    <li class="to-buy__list--item list-element">
+      <span class="list-element__name">${element}</span>  
+      <a class="list-element__delete" href="#">X</a> 
+    </li>
+    `;
+
+  })
+ 
+}
+
+//Function hidding delete-all-button when there is no any elements in list
+function handleVisibleDeleteAllButton() {
+
+  let listElements = document.querySelectorAll('.to-buy__list--item').length;
+  let btnBackground = document.querySelector('.delete-all');
+
+  if (listElements < 1) {
+    deleteBtn.style.display = 'none';
+    btnBackground.style.display = 'none';
+
+  } else {
+    deleteBtn.style.display = 'inline-block'
+    btnBackground.style.display = 'flex';
+  }
+
+};
+
+
+// List manipulations
 toBuyList.addEventListener("click", e => {
   
   //Crossing and uncrossing name of list element
@@ -35,13 +121,14 @@ toBuyList.addEventListener("click", e => {
   else if (e.target.className.includes('to-buy__list--item')) {
     e.target.firstElementChild.classList.toggle('line-through');
   }
+
   
   // Removing list element
   else if(e.target.className === 'list-element__delete') {
-    e.target.parentElement.remove()
+    removeArrayElement(e.target.previousElementSibling.textContent)
   }
 
-  handleDeleteBtnVis();
+  handleVisibleDeleteAllButton();
 
 });
 
@@ -59,18 +146,13 @@ addForm.addEventListener('submit', e => {
   //Input value can't be empty string
   if (input.value !== '') {
 
-    toBuyList.innerHTML += `
-    <li class="to-buy__list--item list-element">
-      <span class="list-element__name">${input.value}</span>  
-      <a class="list-element__delete" href="#">X</a> 
-    </li>
-    `;
+    addArrayElement(input.value);
 
     input.value = '';
 
   }
 
-  handleDeleteBtnVis();
+  handleVisibleDeleteAllButton();
 
 });
 
@@ -89,30 +171,12 @@ popUp.addEventListener('click', e => {
 
   if(e.target.className === 'pop-up' || e.target.className === 'pop-up__window--link-save') {
     popUp.style.display = 'none';
-  } else if (e.target.className === 'pop-up__window--link-delete') {
+  } 
+  else if (e.target.className === 'pop-up__window--link-delete') {
     popUp.style.display = 'none';
-    toBuyList.innerHTML = '';
+    removeAllArrayElements();
   };
 
-  handleDeleteBtnVis();
+  handleVisibleDeleteAllButton();
 
 });
-
-
-//Function responsabile for hidding delete-all-button when there is no any elements in list
-function handleDeleteBtnVis() {
-
-  let listElements = document.querySelectorAll('.to-buy__list--item').length;
-  let btnBackground = document.querySelector('.delete-all');
-
-  if (listElements < 1) {
-    deleteBtn.style.display = 'none';
-    btnBackground.style.display = 'none';
-
-  } else {
-    deleteBtn.style.display = 'inline-block'
-    btnBackground.style.display = 'flex';
-  }
-
-}
-
